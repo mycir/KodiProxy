0105f5c49af359905aefe0820c6afa27b62f5da0
mycir <113249299+mycir@users.noreply.github.com>
2025-09-16 17:32:14 +0100

    Release v1.0 changes

--- app/src/main/java/com/mycir/kodiproxy/MainActivity.kt
+++ app/src/main/java/com/mycir/kodiproxy/MainActivity.kt
@@ -2,19 +2,21 @@ package com.mycir.kodiproxy
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Launch real Kodi (Play Store version)
-        val kodiIntent: Intent? = packageManager.getLaunchIntentForPackage("org.xbmc.kodi")
-        if (kodiIntent != null) {
-            startActivity(kodiIntent)
+        val launchKodi: Intent? = packageManager.getLaunchIntentForPackage("org.xbmc.kodi")
+        launchKodi?.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        if (launchKodi != null) {
+            startActivity(launchKodi)
+            Log.d("KodiProxy", "Kodi launch OK via intent")
+        } else {
+            Log.e("KodiProxy", "Kodi not installed or no launch intent found")
         }
-
-        // Close proxy immediately
         finish()
     }
 }
